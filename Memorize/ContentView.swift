a1a6a9fe6b7553463ed41299458ba4c57488fb24
byxn <fz39588@gmail.com>
2022-10-27 00:33:47 +0800

    feat: Getting started with SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        ZStack(content: {
+//            RoundedRectangle(cornerRadius: 20)
+//                .stroke(lineWidth: 3)
+//            Text("Hello World!")
+//        }).padding(.horizontal).foregroundColor(.red)
+
+//        ZStack(alignment: .center){
+//            RoundedRectangle(cornerRadius: 20)
+//                .stroke(lineWidth: 3)
+//            Text("Hello World!")
+//        }.padding(.horizontal).foregroundColor(.red)
+        
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .stroke(lineWidth: 3)
+            Text("Hello World!")
+        }
+        .padding()
+        .foregroundColor(.primary)
+        
     }
 }
+
+// : some View  不透明类型
+// {...}  是一个方法
+// 函数的省略和kotlin很像
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
